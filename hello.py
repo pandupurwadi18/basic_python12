dab44f5f5bac9d4ef98d6a189caaa96b1aaf6bae
Pandu Purwadi <65107116+pandupurwadi18@users.noreply.github.com>
2022-06-11 21:21:17 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,4 +3,5 @@ print("Python Print Function")
 print("The function declared like below:")
 print('print("What do you want to print")') #When " character in sentences, we can use ' for print function
 # Let use two sentences in one print function and use /n as new line for second sentence
-print('If you want to print "Hello World!"\n print("Hello World!")')
+print('If you want to print "Hello World!"\nprint("Hello World!")') #\n without space make it as new line
+print("I learn Python for \n Data Science \n Web Development \n Game") #\n with space will make different output
