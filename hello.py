cf91e88d127e018bbc21f670f5c7966d88a20aa1
Pandu Purwadi <65107116+pandupurwadi18@users.noreply.github.com>
2022-06-11 21:13:29 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # This is my first program in Python language / Ini adalah program pertama saya menggunakan bahasa pemprograman Python
 print("Python Print Function")
 print("The function declared like below:")
-print('print("What do you want to print")')
-print('If you want to print "Hello World!"')
-print('print("Hello World!")')
+print('print("What do you want to print")') #When " character in sentences, we can use ' for print function
+# Let use two sentences in one print function and use /n as new line for second sentence
+print('If you want to print "Hello World!"\n print("Hello World!")')
