70173d2c8f71c2acfe093978548cfbe68f30c009
Pandu Purwadi <65107116+pandupurwadi18@users.noreply.github.com>
2022-06-11 20:58:53 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-# This is comment / ini adalah komentar
-print("Hello World!") # ini program pertama
+# This is my first program in Python language / Ini adalah program pertama saya menggunakan bahasa pemprograman Python
+print("Python Print Function")
+print("The function declared like below:")
+print('print("What do you want to print")')
+print('If you want to print "Hello World!"')
+print('print("Hello World!")')
