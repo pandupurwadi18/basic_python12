4e1b04c9bc0a811c333e5ebeccfa9634918153ac
unknown <pandupurwadi18@gmail.com>
2022-06-12 15:58:47 +0700

    update Hello World

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,14 @@
 print("Python Print Function")
 print("The function declared like below:")
 print('print("What do you want to print")') #When " character in sentences, we can use ' for print function
+
 # Let use two sentences in one print function and use /n as new line for second sentence
 print('If you want to print "Hello World!"\nprint("Hello World!")') #\n without space make it as new line
 print("I learn Python for \n Data Science \n Web Development \n Game") #\n with space will make different output
+
+#String manipulation
+print("Hello" + "Pandu") #concatenate two string without space
+print("Hello" + " " + "Pandu") #concatenate with space just add new string with space
+
+# print("Hello world!") will cause error "unexpected indent" if before print function, there is a space 
+#print(("Hello world!") will cause error "unexpected EOF while parsing", because second "(" incomplete
